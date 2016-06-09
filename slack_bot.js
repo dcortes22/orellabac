c6290b9b8a4c837da3e7c010b85d0d4bf0ec3288
David Cortes <dcortes22@gmail.com>
2016-06-08 18:39:02 -0600

    First version of botkit picnic

--- slack_bot.js
+++ slack_bot.js
@@ -64,10 +64,10 @@ This bot demonstrates many of the core features of Botkit:
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
 
-if (!process.env.token) {
-    console.log('Error: Specify token in environment');
-    process.exit(1);
-}
+// if (!process.env.token) {
+//     console.log('Error: Specify token in environment');
+//     process.exit(1);
+// }
 
 var Botkit = require('./lib/Botkit.js');
 var os = require('os');
@@ -77,11 +77,11 @@ var controller = Botkit.slackbot({
 });
 
 var bot = controller.spawn({
-    token: process.env.token
+    token: "xoxb-49316833511-oKFBJXkOiXcV491Ar3AXM3pF"
 }).startRTM();
 
 
-controller.hears(['hello', 'hi'], 'direct_message,direct_mention,mention', function(bot, message) {
+controller.hears(['hola'], 'direct_message,direct_mention,mention', function(bot, message) {
 
     bot.api.reactions.add({
         timestamp: message.ts,
@@ -96,14 +96,51 @@ controller.hears(['hello', 'hi'], 'direct_message,direct_mention,mention', funct
 
     controller.storage.users.get(message.user, function(err, user) {
         if (user && user.name) {
-            bot.reply(message, 'Hello ' + user.name + '!!');
+            bot.reply(message, 'Hola ' + user.name + '!!');
         } else {
-            bot.reply(message, 'Hello.');
+            bot.reply(message, 'Hola.');
         }
     });
 });
 
-controller.hears(['call me (.*)', 'my name is (.*)'], 'direct_message,direct_mention,mention', function(bot, message) {
+controller.hears(['cuenteme algo', 'cuenteme un chiste', 'cuente un chiste', 'estoy aburrido'], 'direct_message,direct_mention,mention', function(bot, message){
+  var stories = ['- Mama que haces en frente de la computadora con los hojos cerrados??? \n- Nada hijo es que Windows me dijo que cierre las pestañas',
+                 'Un hombre está haciendo un vuelo en un globo aerostático.' +
+                 'Se extravía y decide descender y preguntar a alguien. Baja a ' +
+                 'unos 10 metros del suelo y pregunta a una persona que pasaba por allí: \n' +
+                 '- Por favor, ¿puede decirme donde estoy?\n' +
+                 '- Pues mire, está usted en un globo aerostático, a unos 10 metros del suelo.\n'+
+                 '- Usted es informático, ¿verdad?\n'+
+                 '- Sí, ¿cómo lo sabe?\n'+
+                 '- Porque me ha dado una respuesta técnicamente correcta, pero que no me soluciona nada.\n'+
+                 '- Y usted es usuario, ¿verdad?\n'+
+                 '- Pues sí, ¿cómo lo sabe?\n'+
+                 '- Porque está igual de perdido que antes, pero ahora me echa la culpa a mí.',
+                 '¿Qué le dice una IP a otra?\n'+
+                 '—¿Qué tramas?', 'http://img.desmotivaciones.es/201508/sexo-silencio-desmotivaciones.jpg',
+                 'http://img.desmotivaciones.es/201112/informatica.jpg', 'https://lh3.googleusercontent.com/-RanqDppqME4/VUOZ06phYnI/AAAAAAAAADQ/q0sTl6Ygrcs/w800-h800/por-esos-chistes-malos-carteles-chistes-tonterias-risa-sonrisa-desmotivaciones.jpg',
+                 'http://img.desmotivaciones.es/201101/images_4701.jpg', 'http://img.desmotivaciones.es/201011/bandaancha.jpg'];
+  var story = stories[Math.floor(Math.random() * stories.length)]
+  bot.reply(message, 'Que tal un chiste?')
+  bot.reply(message, story);
+});
+
+controller.hears(['picnic'], 'ambient', function(bot, message){
+  var userID = message.user;
+  var user = "<@"+userID+">";
+  var reply = user + " hagame mention, no hable a mis espaldas :rage:";
+  bot.reply(message, reply);
+});
+
+controller.hears(['julio', 'orlando', 'orly'], 'direct_message,direct_mention,mention,ambient', function(bot, message){
+  var userID = message.user;
+  var user = "<@"+userID+">";
+  var reply = user + " eso que dijo merece esto";
+  bot.reply(message, reply);
+  bot.reply(message, 'http://gph.is/1HlCG6v');
+});
+
+controller.hears(['me llamo (.*)', 'mi nombre es (.*)', 'soy (.*)'], 'direct_message,direct_mention,mention', function(bot, message) {
     var name = message.match[1];
     controller.storage.users.get(message.user, function(err, user) {
         if (!user) {
@@ -113,24 +150,36 @@ controller.hears(['call me (.*)', 'my name is (.*)'], 'direct_message,direct_men
         }
         user.name = name;
         controller.storage.users.save(user, function(err, id) {
-            bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
+            bot.reply(message, 'Ok. Lo voy a llamar ' + user.name + ' desde ahora.');
         });
     });
 });
 
-controller.hears(['what is my name', 'who am i'], 'direct_message,direct_mention,mention', function(bot, message) {
+controller.on("bot_channel_join", function(bot, message){
+  bot.reply(message, "Ahora si empieza lo bueno :smirk:");
+});
+
+controller.hears(['jale a comer', 'almuerzo', 'comida', 'comen'], 'ambient', function(bot, message){
+  var userID = message.user;
+  var user = "<@"+userID+">";
+  bot.reply(message, "Yo me encargo " + user);
+  bot.say({text: "@channel: hey todos dice " + user + " que tiene hambre a donde vamos",
+           channel:"C11429SQ6"});
+});
+
+controller.hears(['como me llamo', 'quien soy', 'cual es mi nombre'], 'direct_message,direct_mention,mention', function(bot, message) {
 
     controller.storage.users.get(message.user, function(err, user) {
         if (user && user.name) {
-            bot.reply(message, 'Your name is ' + user.name);
+            bot.reply(message, 'Su nombre es ' + user.name);
         } else {
             bot.startConversation(message, function(err, convo) {
                 if (!err) {
-                    convo.say('I do not know your name yet!');
-                    convo.ask('What should I call you?', function(response, convo) {
-                        convo.ask('You want me to call you `' + response.text + '`?', [
+                    convo.say('Aun no se su nombre');
+                    convo.ask('Como debo llamarlo', function(response, convo) {
+                        convo.ask('Quiere que lo llame `' + response.text + '`?', [
                             {
-                                pattern: 'yes',
+                                pattern: 'si',
                                 callback: function(response, convo) {
                                     // since no further messages are queued after this,
                                     // the conversation will end naturally with status == 'completed'
@@ -159,7 +208,7 @@ controller.hears(['what is my name', 'who am i'], 'direct_message,direct_mention
 
                     convo.on('end', function(convo) {
                         if (convo.status == 'completed') {
-                            bot.reply(message, 'OK! I will update my dossier...');
+                            bot.reply(message, 'Ok, he aprendido algo nuevo hoy...');
 
                             controller.storage.users.get(message.user, function(err, user) {
                                 if (!user) {
@@ -169,7 +218,7 @@ controller.hears(['what is my name', 'who am i'], 'direct_message,direct_mention
                                 }
                                 user.name = convo.extractResponse('nickname');
                                 controller.storage.users.save(user, function(err, id) {
-                                    bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
+                                    bot.reply(message, 'Ok lo llamare ' + user.name + ' a partir de ahora');
                                 });
                             });
 
@@ -177,7 +226,7 @@ controller.hears(['what is my name', 'who am i'], 'direct_message,direct_mention
 
                         } else {
                             // this happens if the conversation ended prematurely for some reason
-                            bot.reply(message, 'OK, nevermind!');
+                            bot.reply(message, 'OK, no hay problema');
                         }
                     });
                 }
